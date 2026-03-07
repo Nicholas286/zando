9a4dc7f3511ff1b06a7b153b2ef20695cf454f8c
Nicholas286 <munenenicholas96@gmail.com>
2026-03-07 22:33:34 +0300

    Add root URL redirect to products

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Keep your product routes prefixed with /products/
     path('products/', include('products.urls')),
+    # Redirect root to products
+    path('', lambda request: redirect('products/')),
 ]
 
-# This block allows Django to serve images from your media folder
+# Serve media files only during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
