aa13c70441242d99efc6d308f37a80d3b445933b
Nicholas286 <munenenicholas96@gmail.com>
2026-03-08 13:12:15 +0300

    pyshop.urls added path

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
 path('products/', include('products.urls', namespace='products')),
 path('accounts/', include('django.contrib.auth.urls')),
+path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     # Redirect root to products
     path('', lambda request: redirect('products:index')),
 ]
